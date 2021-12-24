82759f491d6ae160b096e3b1cf29907084a27236
CollinHerber <rustaddicts@gmail.com>
2021-12-23 19:24:21 -0500

    chore: initial route structure and updates

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import { route } from '@aurelia/router';
       path: '',
       component: import('./pages/home/home'),
       title: 'Home',
+    },
+    {
+      path: 'camping',
+      component: import('./pages/camping/camping'),
+      title: 'Camping',
+    },
+    {
+      path: 'news',
+      component: import('./pages/news/news'),
+      title: 'News',
+    },
+    {
+      path: 'lineup',
+      component: import('./pages/lineup/lineup'),
+      title: 'Lineup',
+    },
+    {
+      path: 'info',
+      component: import('./pages/info/info'),
+      title: 'Info',
     }
 ]})
 
