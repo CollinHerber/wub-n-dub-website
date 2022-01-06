2f93c74c188e4fb468e3c20222b6a16f8455807d
Collin Herber <rustaddicts@gmail.com>
2022-01-05 22:05:57 -0500

    chore: various updates and layout changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Aurelia, { RouterConfiguration } from 'aurelia';
 import { AllConfiguration } from '@aurelia-mdc-web/all';
 import { App } from './app';
+import { WubNavbar } from "./resources/elements/wub-navbar/wub-navbar";
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 Aurelia
-    .register(RouterConfiguration, AllConfiguration)
+    .register(RouterConfiguration, AllConfiguration, WubNavbar)
     .app(App)
     .start();
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,3 +1,9 @@
 export class Home {
-
+    newsEvents = [
+        "Post 1",
+        "Post 2",
+        "Post 3",
+        "Post 4",
+        "Post 5"
+    ]
 }
