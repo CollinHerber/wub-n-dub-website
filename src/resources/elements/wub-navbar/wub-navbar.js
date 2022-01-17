1b6d1e49d474d076c6fd0cf29b887706fb4de790
CollinHerber <rustaddicts@gmail.com>
2022-01-17 12:53:34 -0500

    chore: mobile responsiveness updates

--- src/resources/elements/wub-navbar/wub-navbar.js
+++ src/resources/elements/wub-navbar/wub-navbar.js
@@ -3,10 +3,12 @@ export class WubNavbar {
     lastScrollPosition;
 
     attached() {
-        document.addEventListener("scroll", () => { // or window.addEventListener("scroll"....
-            const st = window.scrollY || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-            this.showNav = st < this.lastScrollPosition;
-            this.lastScrollPosition = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-        });
+        setTimeout( () => {
+            document.addEventListener("scroll", () => { // or window.addEventListener("scroll"....
+                const st = window.scrollY || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+                this.showNav = st < this.lastScrollPosition;
+                this.lastScrollPosition = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+            });
+        }, 100)
     };
 }
